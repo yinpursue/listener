1f4c6231d3599be4e463632a3af34483a6b45c60
yinzhenzhao <yinpursue@163.com>
2018-12-04 15:19:31 +0800

    7

--- src/main/java/cn/yin/listener/HelloWorld.java
+++ src/main/java/cn/yin/listener/HelloWorld.java
@@ -2,6 +2,7 @@ package cn.yin.listener;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("555");
+        System.out.println("666");
+
     }
 }
