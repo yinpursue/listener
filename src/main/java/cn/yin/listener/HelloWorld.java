00b7e06d35ab60863c5cc068eb4c22f4f5438261
yinzhenzhao <yinpursue@163.com>
2018-12-04 15:56:54 +0800

    8

--- src/main/java/cn/yin/listener/HelloWorld.java
+++ src/main/java/cn/yin/listener/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("666");
         System.out.println("need delete");
+        System.out.println("ddd");
     }
 }
