14f2f7903227a1205d35ed6201f7c28f40be1728
yinzhenzhao <yinpursue@163.com>
2018-12-04 15:34:01 +0800

    8

--- src/main/java/cn/yin/listener/HelloWorld.java
+++ src/main/java/cn/yin/listener/HelloWorld.java
@@ -3,6 +3,6 @@ package cn.yin.listener;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("666");
-
+        System.out.println("need delete");
     }
 }
